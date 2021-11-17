61c61888a7e18d6b625ef8df63d6153a210ca221
hyeSUon <hyeonsu@kpu.ac.kr>
2021-11-17 21:13:19 +0900

    전반적 이미지 수정
    마리오 애니메이션 간격 일정하게 수정
    실제 게임과 같은 비율 계산

---
+++ Supermario0.11v/main_state.py
@@ -0,0 +1,63 @@
+import random
+import json
+import os
+import title_state
+
+import player
+
+from pico2d import *
+
+import game_framework
+
+
+name = "MainState"
+mario = None
+
+def enter():
+    mario = player.Mario()
+
+def exit():
+    global mario
+    del(mario)
+    close_canvas()
+
+def pause():
+    pass
+
+
+def resume():
+    pass
+
+
+def handle_events():
+    global mario
+    events = get_events()
+    for event in events:
+        #종료버튼
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_ESCAPE:
+                game_framework.quit()
+        #좌우이동
+            elif event.key == SDLK_RIGHT:
+                mario.dir += 1
+            elif event.key == SDLK_LEFT:
+                mario.dir -= 1
+        elif event.type == SDL_KEYUP:
+            if event.key == SDLK_RIGHT:
+                mario.dir -= 1
+            elif event.key == SDLK_LEFT:
+                mario.dir += 1
+
+
+def update():
+    global mario
+    mario.update()
+    delay(0.01)
+
+def draw():
+    clear_canvas()
+    mario.draw()
+    update_canvas()
+
--- Supermario0.11v/mygame.py
+++ Supermario0.11v/mygame.py
@@ -1,8 +1,8 @@
 import game_framework
 import pico2d
-
+import option
+import start_state
 import main_state
-
-pico2d.open_canvas(768, 480)
+pico2d.open_canvas(option.CANVAS_WIDTH, option.CANVAS_HEIGHT)
 game_framework.run(main_state)
 pico2d.close_canvas()
