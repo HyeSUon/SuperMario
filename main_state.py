504e683d412eb4a9c0f9aa6620ef4e5a103fcad3
hyeSUon <hyeonsu@kpu.ac.kr>
2021-12-07 04:03:17 +0900

    main, title 구현
    다음 할일
    mario, server, game_world 추가하고 score_board 구현
    다다음 할일
    스크롤링? 배경 구현

---
+++ main_state.py
@@ -0,0 +1,32 @@
+import random
+import os
+
+from pico2d import *
+import game_framework
+
+name = "MainState"
+
+def enter():
+    pass
+
+def pause():
+    pass
+
+def resume():
+    pass
+
+def handle_events():
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            game_framework.quit()
+        else:
+            pass
+def update():
+    pass
+
+def draw():
+    clear_canvas()
+    update_canvas()
---
+++ title_state.py
@@ -0,0 +1,56 @@
+import game_framework
+from pico2d import *
+import json
+name = "TitleState"
+image = None
+score = None
+
+# 숫자 좌표값 불러오기
+with open('json//number.json', 'r') as f:
+    number = json.load(f)
+
+def enter():
+    global image, score
+    image = load_image('images//Title.png')
+    score = load_image('images//char.png')
+
+def exit():
+    global image, score
+    del(image)
+    del(score)
+import main_state
+
+def handle_events():
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        else:
+            if(event.type, event.key) == (SDL_KEYDOWN, SDLK_ESCAPE):
+                game_framework.quit()
+            elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_SPACE):
+                game_framework.change_state(main_state)
+def draw():
+    clear_canvas()
+    image.draw(400, 300)
+    for i in range (0,6):
+        score.clip_draw(number[0]['x'], number[0]['y'],
+                    number[0]['w'], number[0]['h'], 75+number[0]['w']/2 +26*i, 559-number[0]['h']/2, 26, 22)
+    for i in range (0,6):
+        score.clip_draw(number[0]['x'], number[0]['y'],
+                    number[0]['w'], number[0]['h'], 426+number[0]['w']/2 +26*i, 128-number[0]['h']/2, 26, 22)
+    for i in range (0,2):
+        score.clip_draw(number[0]['x'], number[0]['y'],
+                    number[0]['w'], number[0]['h'], 326+number[0]['w']/2 +26*i, 559-number[0]['h']/2, 26, 22)
+
+    score.clip_draw(number[1]['x'], number[1]['y'],
+                    number[1]['w'], number[1]['h'], 475 + number[1]['w'] / 2, 559 - number[1]['h']/2, 26, 22)
+
+    update_canvas()
+
+def update():
+    pass
+def pause():
+    pass
+def resume():
+    pass
