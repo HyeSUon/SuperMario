e04dbea7c735de92301c8c36a9ac298662f9ffda
hyeSUon <hyeonsu@kpu.ac.kr>
2021-12-08 20:30:30 +0900

    Revert "수정실패"
    This reverts commit ecfff37c9f5db55678b841bb6701f03e19a52eb5.

--- score.py
+++ score.py
@@ -1,12 +1,29 @@
 from pico2d import *
 
 import server
+import json
 
 
 class Score:
-    def __init__(self):
-        #self.image = load_image('images//char.png')
-        pass
 
-    def st(self, name):
-        print(name)
+    def __init__(self):
+        self.image = load_image('images\char.png')
+        with open('json//number.json', 'r') as f:
+            self.number = json.load(f)
+
+
+    def point_draw(self, name):
+        n = 0
+        for i in range (0,6):
+            s = n%10
+            n %= 10
+            self.image.clip_draw(self.number[s]['x'], self.number[s]['y'],
+                        self.number[s]['w'], self.number[s]['h'], 205+self.number[s]['w']/2-26*i, 559-self.number[s]['h']/2, 26, 22)
+    def coin_draw(self):
+        pass
+    def time_draw(self):
+        pass
+    def top_score_draw(self):
+        pass
+    def life_draw(self):
+        pass
